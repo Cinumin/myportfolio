6a6a2831cbc0109776099c72fcd3da9a58c7e924
Cinumin <angelacutie998@gmail.com>
2026-01-14 10:15:41 -0500

    Update logo image and add custom cursor
    Replaces the React logo with an external GIF in App.jsx and adds a custom cursor using watering_can.png in App.css. Removes unused CSS related to logo animation and card styling.

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
    <div>
         <h1 id="welcome"> Hello </h1>
        <a href="https://www.lovethispic.com/uploaded_images/43771-Blossom-Gif.gif?1" target="_blank">
-         <img src={reactLogo} className="logo react" alt="React logo" />
+         <img src="https://www.lovethispic.com/uploaded_images/43771-Blossom-Gif.gif?1" className="logo react" alt="React logo" />
        </a>
      </div>
   )
