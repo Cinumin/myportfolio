98debf2ee4c9e31d61528d11455df2650b09bb8b
Cinumin <angelacutie998@gmail.com>
2026-01-14 17:47:21 -0500

    Add assets and scaffold new pages
    Added watering_can.png and watering_can.svg to the public directory. Created empty files for new pages (About, Contact, Home, Projects) and a lemon asset. Updated App.css and App.jsx, and modified vite.config.js, likely to support new assets or routing. These changes set up the structure for new content and visual assets.

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,17 @@ function App() {
 
   return (
    <div>
-        <h1 id="welcome"> Hello </h1>
+        <h1 id="main_title"> Angela Zhao </h1>
        <a href="https://www.lovethispic.com/uploaded_images/43771-Blossom-Gif.gif?1" target="_blank">
          <img src="https://www.lovethispic.com/uploaded_images/43771-Blossom-Gif.gif?1" className="logo react" alt="React logo" />
        </a>
-     </div>
-  )
-}
 
-export default App
+       <div class="container">
+            <div class="column pointer"><a id="link">Hover this!</a></div>
+            <div class="column drag"></div>
+            <div class="column zoom"></div>
+            <div class="column not-allowed"></div>
+        </div>
+  </div>
+)}
+  export default App
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: "myportfolio",
 })
-
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
-
-@import "tailwindcss";
